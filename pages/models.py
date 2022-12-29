67de1a4e465bd6bb6bb868b84db4c2230eab1659
thisisjab <heythisisjab@gmail.com>
2022-12-29 16:39:32 +0330

    Create Contact model and do migrations

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=255, null=False, blank=False)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
