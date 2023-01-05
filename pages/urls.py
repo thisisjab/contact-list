4a18b6b2116639c37e616233474aefe92f50d85e
thisisjab <heythisisjab@gmail.com>
2023-01-05 17:20:14 +0330

    Config individual Contact url and related view and template - Also update individual contact link in index.html file

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ContactDetailView
 
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='homepage')
+    path('contact/<int:pk>', ContactDetailView.as_view(), name='contact_detail'),
+    path('', HomePageView.as_view(), name='homepage'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Contact
 
 
 class HomePageView(ListView):
     template_name = 'index.html'
+    model = Contact
+
+
+class ContactDetailView(DetailView):
+    template_name = 'contact.html'
     model = Contact
